ecb982ad5248e3670ea6238f7e61d2f4bf26ca78
Emmet Delaney <emmet.delaney@sitenna.com>
2024-10-16 14:39:16 +0100

    hardware vm

---
+++ systems/aarch64-linux/nixos/default.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  imports = [./hardware.nix];
+
+  system.boot.efi.enable = true;
+
+  apps = {
+    misc.enable = true;
+    tools.git.enable = true;
+  };
+
+  suites.common.enable = true; # Enables the basics, like audio, networking, ssh, etc.
+
+  system.stateVersion = "22.11";
+}
