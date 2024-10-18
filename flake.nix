84b7b149ff60dbf16594da0cf34c6a1b4c91d5ae
Emmet Delaney <emmet.delaney@sitenna.com>
2024-10-18 15:57:28 +0100

    shits & santa

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Core
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     snowfall-lib = {
