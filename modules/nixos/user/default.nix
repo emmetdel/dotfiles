23d579c25167591fd8c8a7586088a6a9a7130f63
Emmet Delaneu <emmetdel@gmail.com>
2024-10-21 21:40:10 +0100

    change the fullname

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -1,9 +1,8 @@
-{
-  options,
-  config,
-  pkgs,
-  lib,
-  ...
+{ options
+, config
+, pkgs
+, lib
+, ...
 }:
 with lib;
 with lib.custom; let
@@ -19,30 +18,34 @@ with lib.custom; let
       cp $src $out
     '';
 
-    passthru = {fileName = defaultIconFileName;};
+    passthru = { fileName = defaultIconFileName; };
   };
   propagatedIcon =
     pkgs.runCommandNoCC "propagated-icon"
-    {passthru = {inherit (cfg.icon) fileName;};}
-    ''
-      local target="$out/share/icons/user/${cfg.name}"
-      mkdir -p "$target"
+      { passthru = { inherit (cfg.icon) fileName; }; }
+      ''
+        local target="$out/share/icons/user/${cfg.name}"
+        mkdir -p "$target"
 
-      cp ${cfg.icon} "$target/${cfg.icon.fileName}"
-    '';
-in {
+        cp ${cfg.icon} "$target/${cfg.icon.fileName}"
+      '';
+in
+{
   options.user = with types; {
     name = mkOpt str "emmetdelaney" "The name to use for the user account.";
     initialPassword =
       mkOpt str "password"
-      "The initial password to use when the user is first created.";
+        "The initial password to use when the user is first created.";
     icon =
       mkOpt (nullOr package) defaultIcon
-      "The profile picture to use for the user.";
-    extraGroups = mkOpt (listOf str) [] "Groups for the user to be assigned.";
+        "The profile picture to use for the user.";
+    extraGroups = mkOpt (listOf str) [ ] "Groups for the user to be assigned.";
     extraOptions =
-      mkOpt attrs {}
-      "Extra options passed to <option>users.users.<name></option>.";
+      mkOpt attrs { }
+        "Extra options passed to <option>users.users.<name></option>.";
+
+    # Added fullname option
+    fullname = mkOpt (nullOr str) "Emmet Delaney" "The full name of the user.";
   };
 
   config = {
@@ -70,12 +73,12 @@ in {
     users.users.${cfg.name} =
       {
         isNormalUser = true;
-        inherit (cfg) name initialPassword;
+        inherit (cfg) name initialPassword fullname;
         home = "/home/${cfg.name}";
         group = "users";
 
         extraGroups =
-          ["wheel" "audio" "sound" "video" "networkmanager" "input" "tty" "docker"]
+          [ "wheel" "audio" "sound" "video" "networkmanager" "input" "tty" "docker" ]
           ++ cfg.extraGroups;
       }
       // cfg.extraOptions;
